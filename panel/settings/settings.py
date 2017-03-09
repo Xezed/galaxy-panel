dddc55d72909494e5f1abd24a5390efdfd8ad7fd
Alexandr Zakharov <sasha750i@mail.ru>
2017-03-09 13:10:25 +0300

    Registration page template

--- panel/settings/settings.py
+++ panel/settings/settings.py
@@ -120,6 +120,7 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_URL = '/static/'
 
 
