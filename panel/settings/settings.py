2c445a04275b97403bfb6bb95c503e767705c4e3
Alexandr Zakharov <sasha750i@mail.ru>
2017-03-09 13:54:30 +0300

    Email backend

--- panel/settings/settings.py
+++ panel/settings/settings.py
@@ -151,9 +151,10 @@ CUSER_SETTINGS = {
 
 
 # for dev
-EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_PORT = os.environ['EMAIL_PORT']
-EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = os.environ['EMAIL_HOST']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+# EMAIL_PORT = os.environ['EMAIL_PORT']
+# EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
 
