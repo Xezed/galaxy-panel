f54b1fd4616aca70e5401122662edb76bf85f20d
Alexandr Zakharov <sasha750i@mail.ru>
2017-03-10 22:52:49 +0300

    Email galaxydata settings

--- panel/settings/settings.py
+++ panel/settings/settings.py
@@ -152,9 +152,14 @@ CUSER_SETTINGS = {
 
 # for dev
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_PORT = os.environ['EMAIL_PORT']
-EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+# EMAIL_HOST = os.environ['EMAIL_HOST']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+# EMAIL_PORT = os.environ['EMAIL_PORT']
+# EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+EMAIL_HOST = 'galaxydata.ru'
+EMAIL_HOST_PASSWORD = 'xHn123e'
+EMAIL_HOST_USER = 'dev@galaxydata.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
